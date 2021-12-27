2115ac1ead7c82827ff9058163eb2cdd28874bd1
unknown <hardiyanto.nugroho@gmail.com>
2021-12-27 12:15:21 +0700

    pertemuan 10 praktik 1 & 2

---
+++ pertemuan10/koneksi.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'wpsmt5';
+
+//koneksi ke DB
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// query tabel calom_mhs
+$result = mysqli_query($conn, 'SELECT * FROM calon_mhs');
+
+// AMBIL data dari tabel calon mhs
+// mysqli_fetch_row(); mengembalikan data dalam bentuk numerik
+// mysqli_fetch_assoc(); mengembalikan data dalam nama field
+// mysqli_fetch_array(); mengembalikan data dalam bentuk numerik dan nama field
+
+// while ($camaba = mysqli_fetch_row($result)){
+// var_dump($camaba);
+// }
---
+++ pertemuan10/view.php
@@ -0,0 +1,130 @@
+<?php
+// date_default_timezone_get("Asia/Jakarta");
+
+include 'koneksi.php';
+
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+$mahasiswa = $rows;
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="tema/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- font -->
+  <link rel="stylesheet" href="tema/fontawesome/css/all.min.css">
+
+  <title>CRUD</title>
+</head>
+
+<body>
+  <!-- navbar -->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#">SIPEMABA || Kampus Kita</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        </ul>
+        <div class="text-white">
+          <?php echo date('l, d-m-y'); ?>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <!-- sidebar -->
+
+  <div class="row mt-4">
+    <div class="col-md-2 mt-2 pr-3 pt-4 bg-secondary">
+      <!-- menu -->
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+          <hr class="bg-dark">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fas fa-users"></i> calon Mahasiswa</a>
+          <hr>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fas fa-user-edit"></i> Input camaba</a>
+          <hr>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fas fa-chalkboard-teacher"></i> Daftar Dosen</a>
+          <hr>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fas fa-calendar-alt"></i> jadwal Kuliah</a>
+          <hr>
+        </li>
+
+      </ul>
+    </div>
+    <div class="col-md-10 p-5 pt-5">
+      <!-- konten -->
+      <h3><i class="fas fa-users"></i> Daftar Calon Mahasiswa</h3>
+      <hr>
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama Lengkap</th>
+            <th scope="col">Alamat</th>
+            <th scope="col">Jenis Kelamin</th>
+            <th scope="col">agama</th>
+            <th scope="col">sekolah Asal</th>
+            <th scope="col">Foto</th>
+            <th scope="col">Opsi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $no = 1; ?>
+          <?php foreach ($mahasiswa as $cama) : ?>
+            <tr>
+              <th scope="row"><?php echo $no; ?></th>
+              <td><?= $cama['nama']; ?></td>
+              <td><?= $cama['alamat']; ?></td>
+              <td><?= $cama['jenis_kelamin']; ?></td>
+              <td><?= $cama['agama']; ?></td>
+              <td><?= $cama['sekolah_asal']; ?></td>
+              <td><img src="image/<?= $cama['foto_maba']; ?> " width="100px"></td>
+              <td><i class="fas fa-edit"></i>|| <i class="fas fa-trash"></i></td>
+            </tr>
+            <?php $no++ ?>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
